5104cb2dc17f4e296dc4461fd6982d609e5ef101
panabar <32486413+panabar@users.noreply.github.com>
2022-11-04 17:42:34 +0000

    added a minor line

--- hello/hello.c
+++ hello/hello.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
 
     printf("Hello, I'm panabar!\n");
     printf("This is Computer Science I!'n");
+    printf("Minor is Project Management.\n");
 
     return 0;
 }
